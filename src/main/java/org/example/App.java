ae91b8c87dccebe5f990563d46c7e6d576916193
Professional <volk1119229@gmail.com>
2021-05-30 16:20:59 +0300

    cec

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
-        primaryStage.setTitle("Triangle");
+        primaryStage.setTitle("Reverse");
         primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.setResizable(false);
         primaryStage.show();
